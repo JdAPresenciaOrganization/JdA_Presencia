1ef8f5a6e3f45797ddf51da80d6769edd5089f8e
andreivg <cf17andrei.gindac@iesjoandaustria.org>
2019-11-20 21:56:30 +0100

    Login en mvp

---
+++ app/src/main/java/com/example/jdapresencia/LoginActivity.java
@@ -0,0 +1,57 @@
+package com.example.jdapresencia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.jdapresencia.model.LoginPresenterImpl;
+import com.example.jdapresencia.presenter.LoginPresenter;
+import com.example.jdapresencia.view.LoginView;
+
+public class LoginActivity extends AppCompatActivity implements LoginView {
+
+    //Variables
+    EditText user, pass;
+    Button bLogin;
+
+    LoginPresenter mLoginPresenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+
+        //Recoger valores del layout en variables
+        user = findViewById(R.id.username);
+        pass = findViewById(R.id.password);
+        bLogin = findViewById(R.id.login_button);
+
+        mLoginPresenter = new LoginPresenterImpl(LoginActivity.this);
+
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mLoginPresenter.checkLogin(user.getText().toString(), pass.getText().toString());
+            }
+        });
+    }
+
+    @Override
+    public void loginValidations() {
+        Toast.makeText(getApplicationContext(), "Enter username and password",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void loginSuccess() {
+        Toast.makeText(getApplicationContext(), "Login OK",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void loginError() {
+        Toast.makeText(getApplicationContext(), "Login not ok",Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/example/jdapresencia/view/LoginView.java
@@ -0,0 +1,8 @@
+package com.example.jdapresencia.view;
+
+public interface LoginView {
+
+    void loginValidations();
+    void loginSuccess();
+    void loginError();
+}
