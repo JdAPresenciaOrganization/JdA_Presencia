0784e1df6912e4d91059bd90240e232e47e03c3c
banderin96 <sbanderas@iesjoandaustria.org>
2019-11-21 19:33:18 +0100

    nav_menu commit

--- app/src/main/java/com/example/jdapresencia/ui/buscador_trabajadores/BuscadorTrabajadoresViewModel.java
+++ app/src/main/java/com/example/jdapresencia/ui/buscador_trabajadores/BuscadorTrabajadoresViewModel.java
@@ -1,16 +1,16 @@
-package com.example.jdapresencia.ui.slideshow;
+package com.example.jdapresencia.ui.buscador_trabajadores;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class SlideshowViewModel extends ViewModel {
+public class BuscadorTrabajadoresViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public SlideshowViewModel() {
+    public BuscadorTrabajadoresViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is slideshow fragment");
+        mText.setValue("Este es el buscador de trabajadores");
     }
 
     public LiveData<String> getText() {
