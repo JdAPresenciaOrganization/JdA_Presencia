c689f204c120af95cd8b54c9a041711a6d132750
andreivg <cf17andrei.gindac@iesjoandaustria.org>
2019-11-21 19:13:29 +0100

    Navigation drawer icons

--- app/src/main/java/com/example/jdapresencia/ui/gestionar_trabajadores/GestionarTrabajadoresViewModel.java
+++ app/src/main/java/com/example/jdapresencia/ui/gestionar_trabajadores/GestionarTrabajadoresViewModel.java
@@ -1,16 +1,16 @@
-package com.example.jdapresencia.ui.gallery;
+package com.example.jdapresencia.ui.gestionar_trabajadores;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+public class GestionarTrabajadoresViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public GalleryViewModel() {
+    public GestionarTrabajadoresViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+        mText.setValue("This is gestionar trabajadores fragment");
     }
 
     public LiveData<String> getText() {
