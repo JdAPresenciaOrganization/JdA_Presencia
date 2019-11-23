caedfa8dabb303efacf2d57cc5ad20e0b141aa45
andreivg <cf17andrei.gindac@iesjoandaustria.org>
2019-11-23 15:28:47 +0100

    login frontend

--- app/src/main/java/com/example/jdapresencia/LoginActivity.java
+++ app/src/main/java/com/example/jdapresencia/LoginActivity.java
@@ -24,7 +24,7 @@ public class LoginActivity extends AppCompatActivity implements LoginView {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_activity);
+        setContentView(R.layout.activity_login);
 
         //Recoger valores del layout en variables
         user = findViewById(R.id.username);
