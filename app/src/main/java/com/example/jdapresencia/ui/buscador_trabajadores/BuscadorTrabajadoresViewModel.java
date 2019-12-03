e327289264158fbe4d2c4b1f00c2af007174fa16
banderin96 <sbanderas@iesjoandaustria.org>
2019-12-03 19:43:05 +0100

    commit buscador trabajadores viewmodel

--- app/src/main/java/com/example/jdapresencia/ui/buscador_trabajadores/BuscadorTrabajadoresViewModel.java
+++ app/src/main/java/com/example/jdapresencia/ui/buscador_trabajadores/BuscadorTrabajadoresViewModel.java
@@ -7,14 +7,23 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import android.util.Log;
+
+import com.example.jdapresencia.LoginActivity;
+import com.example.jdapresencia.model.LoginPresenterImpl;
 import com.example.jdapresencia.model.User;
+import com.example.jdapresencia.presenter.LoginPresenter;
 
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+
+import static com.example.jdapresencia.LoginActivity.FILE_NAME;
 
 public class BuscadorTrabajadoresViewModel extends ViewModel {
 
@@ -28,7 +37,6 @@ public class BuscadorTrabajadoresViewModel extends ViewModel {
         //mText.setValue("Este es el buscador de trabajadores");
 
 
-
     }
 
     public void pasarContexto(Context context) {
@@ -38,48 +46,52 @@ public class BuscadorTrabajadoresViewModel extends ViewModel {
 
     public ArrayList<User> getUsersBy( String campo, String valor) {
 
-        ArrayList<User> usersArray = new ArrayList<>();
+        File file = new File(context.getFilesDir().getPath()+FILE_NAME);
+        ArrayList<User> allusers = new ArrayList<>();
+        ArrayList<User> customUsersArray = new ArrayList<>();
+
+
 
         try {
-            String FILE_NAME = "/usersFile.dat";
 
-            File file = new File(context.getFilesDir().getPath()+FILE_NAME);
             FileInputStream filein = new FileInputStream(file);
-            ObjectInputStream entrada = new ObjectInputStream(filein);
+            ObjectInputStream dataIN = new ObjectInputStream(filein);
+            allusers = (ArrayList<User>) dataIN.readObject();
 
-            User usuario = (User) entrada.readObject();
+             for(int i = 0;i < allusers.size(); i++) {
 
-            while (usuario!=null) {
+                 switch (campo) {
+                     case "idU":
+                         if (allusers.get(i).getIdU().equals(valor)) {
+                             customUsersArray.add(allusers.get(i));
+                         }
+                         break;
+                     case "rol":
+                         if (allusers.get(i).getRol().equals(valor)) {
+                             customUsersArray.add(allusers.get(i));
+                         }
+                         break;
+                     case "username":
+                         if (allusers.get(i).getUsername().equals(valor)) {
+                             customUsersArray.add(allusers.get(i));
+                         }
+                         break;
+                     default:
+                         customUsersArray = allusers;
+                 }
+             }
 
-                switch (campo) {
-                    case "idU":
-                        if (usuario.getIdU().equals(valor)) {
-                            usersArray.add(usuario);
-                        }
-                        break;
-                    case "rol":
-                        if (usuario.getRol().equals(valor)) {
-                            usersArray.add(usuario);
-                        }
-                        break;
-                    case "username":
-                        if (usuario.getUsername().equals(valor)) {
-                            usersArray.add(usuario);
-                        }
-                        break;
-                    default:
-                        usersArray.add(usuario);
-                }
-
-                usuario = (User) entrada.readObject();
-
-            }
-            entrada.close();
-        } catch (IndexOutOfBoundsException | NullPointerException | IOException | ClassNotFoundException e) {
+        } catch (IndexOutOfBoundsException | NullPointerException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
 
-        return usersArray;
+        return customUsersArray;
     };
 
     //public LiveData<String> getText() {
