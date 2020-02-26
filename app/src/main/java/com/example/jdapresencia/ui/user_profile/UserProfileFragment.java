50f28ca508e80ef4b686b74a2fccd3fdeaa3d25f
Andrei <andrei.gindac@in2.es>
2020-02-26 08:26:31 +0100

    User take selfie with camara

--- app/src/main/java/com/example/jdapresencia/ui/user_profile/UserProfileFragment.java
+++ app/src/main/java/com/example/jdapresencia/ui/user_profile/UserProfileFragment.java
@@ -2,21 +2,35 @@ package com.example.jdapresencia.ui.user_profile;
 
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.example.jdapresencia.R;
+
+import static android.app.Activity.RESULT_OK;
 
 public class UserProfileFragment extends Fragment {
 
     private UserProfileViewModel userProfileViewModel;
+
+    ImageView imageView;
+    ImageButton bUpload, bFoto;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    Bitmap imageBitmap = null;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -25,6 +39,34 @@ public class UserProfileFragment extends Fragment {
                 ViewModelProviders.of(this).get(UserProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_user_profile, container, false);
 
+        imageView = root.findViewById(R.id.imageView);
+        bUpload = root.findViewById(R.id.uploadImageButton);
+        bFoto = root.findViewById(R.id.fotoImageButton);
+
+        bFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+                    startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+
         return root;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+        }
+
+        if (imageBitmap != null) {
+            imageView.setImageBitmap(imageBitmap);
+        }
     }
 }
