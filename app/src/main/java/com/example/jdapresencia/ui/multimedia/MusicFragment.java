b7ca25bb17223571e41ff4d5c71dbbabe7ba8405
Andrei <andrei.gindac@in2.es>
2020-02-28 08:54:34 +0100

    Media player music

--- app/src/main/java/com/example/jdapresencia/ui/multimedia/MusicFragment.java
+++ app/src/main/java/com/example/jdapresencia/ui/multimedia/MusicFragment.java
@@ -2,6 +2,7 @@ package com.example.jdapresencia.ui.multimedia;
 
 import androidx.lifecycle.ViewModelProviders;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.jdapresencia.R;
 
@@ -18,12 +20,47 @@ public class MusicFragment extends Fragment {
 
     private MusicViewModel musicViewModel;
 
+    Button play, pause;
+    MediaPlayer mediaPlayer;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         musicViewModel =
                 ViewModelProviders.of(this).get(MusicViewModel.class);
         View root = inflater.inflate(R.layout.music_fragment, container, false);
-        return inflater.inflate(R.layout.music_fragment, container, false);
+
+
+        play = root.findViewById(R.id.buttonPlay);
+        pause = root.findViewById(R.id.buttonStop);
+
+        //Cargo el fichero a reproducir
+        mediaPlayer = MediaPlayer.create(getContext(), R.raw.ahriximaginenocopyrightmusic);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Gestiono el Reproductor multimedia
+                mediaPlayer.start();
+                mediaPlayer.setLooping(true);
+            }
+        });
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Gestiono el Reproductor multimedia
+                mediaPlayer.pause();
+            }
+        });
+
+        return root;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        //Libero los recursos del reproductor multimedia
+        mediaPlayer.release();
     }
 }
